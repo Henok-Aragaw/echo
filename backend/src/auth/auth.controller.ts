6bd8adfb69616e8478517cacf308eafab602e776
Henok-Aragaw <henokaragaw17@gmail.com>
2025-12-17 05:13:19 -0900

    fix auth controller import issue

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import { All, Controller, Req, Res } from '@nestjs/common';
-import { toNodeHandler } from 'better-auth/node';
 import { auth } from '../lib/auth';
 
 @Controller('auth')
 export class AuthController {
     @All('*path')
     async handleAuth(@Req() req, @Res() res) {
-        return toNodeHandler(auth)(req, res)
+        const { toNodeHandler } = await import('better-auth/node');
+        
+        return toNodeHandler(auth)(req, res);
     }
 }
