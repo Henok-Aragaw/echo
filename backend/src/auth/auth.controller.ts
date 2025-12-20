71e3772dc7fb9e894a1f5645354e5512a7ac0701
Henok-Aragaw <henokaragaw17@gmail.com>
2025-12-20 10:47:26 -0900

    fix dynamic import issue

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -5,6 +5,18 @@ import { getAuth } from '../lib/auth';
 export class AuthController {
     @All('*path')
     async handleAuth(@Req() req, @Res() res) {
+ 
+        try {
+            require.resolve("better-auth");
+            require.resolve("better-auth/node");
+            require.resolve("better-auth/adapters/prisma");
+            require.resolve("better-auth/plugins");
+            require.resolve("better-auth/api");
+            require.resolve("@better-auth/expo"); 
+        } catch (e) {
+            
+        }
+
         const _importDynamic = new Function('modulePath', 'return import(modulePath)');
         
         const { toNodeHandler } = await _importDynamic('better-auth/node');
